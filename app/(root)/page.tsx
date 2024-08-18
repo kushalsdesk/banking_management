abb579522483db577157423a14c6b44c5236c234
kushalsdesk <kushal99mondal@gmail.com>
2024-08-18 21:36:37 +0530

    implement modified homepage
    with headerbox and totalbalancebox inside

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,28 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { FirstName: "Kushal" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.FirstName || "Guest"}
+            subtext="Access and Manage all transcations efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.39}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
