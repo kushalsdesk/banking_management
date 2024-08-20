61fd085ced29e795b24257b49e4e1c84e969badd
kushalsdesk <kushal99mondal@gmail.com>
2024-08-20 23:15:34 +0530

    add intial sidebar commit
    started the sidebar and also place the component inside root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Kusal", lastName: "Mondal" };
   return (
-    <main>
-      SideBar
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
       {children}
     </main>
   );
---
+++ components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  return (
+    <>
+      <section className="sidebar">
+        <nav className="flex flex-col gap-4">
+          <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+            <Image
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="Horizon logo"
+              className="size-[24px] max-xl:size-14"
+            />
+            <h1 className="sidebar-logo">Horizon</h1>
+          </Link>
+        </nav>
+      </section>
+      <div></div>
+    </>
+  );
+};
+
+export default Sidebar;
