8265088dcaa6d3801588c0a7a4f191b2e28c15c5
kushalsdesk <kushal99mondal@gmail.com>
2024-08-20 22:55:51 +0530

    remove code issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,14 +17,11 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout(
-  {
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>,
-  p0: any,
-) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
       <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
