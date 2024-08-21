b696bc4bad00db7f11e8407243f927a8f45e6460
kushalsdesk <kushal99mondal@gmail.com>
2024-08-21 09:51:17 +0530

    add desktop-friendly sidebar
    sidebar elements are styled and ready for large viewports

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,13 +1,21 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Sidebar = ({ user }: SiderbarProps) => {
+  const pathName = usePathname();
   return (
     <>
       <section className="sidebar">
         <nav className="flex flex-col gap-4">
-          <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+          <Link
+            href="/"
+            className="mb-12 flex cursor-pointer items-center gap-2"
+          >
             <Image
               src="/icons/logo.svg"
               width={34}
@@ -17,7 +25,38 @@ const Sidebar = ({ user }: SiderbarProps) => {
             />
             <h1 className="sidebar-logo">Horizon</h1>
           </Link>
+          {sidebarLinks.map((item) => {
+            const isActive =
+              pathName === item.route || pathName.startsWith(`${item.route}/`);
+            return (
+              <Link
+                href={item.route}
+                key={item.label}
+                className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+              >
+                <div className="relative size-6">
+                  <Image
+                    className={cn({
+                      "brightness-[3] invert-0": isActive,
+                    })}
+                    src={item.imgURL}
+                    alt={item.label}
+                    fill
+                  />
+                </div>
+                <p
+                  className={cn("sidebar-label", {
+                    "!text-white": isActive,
+                  })}
+                >
+                  {item.label}
+                </p>
+              </Link>
+            );
+          })}
+          User
         </nav>
+        Footer
       </section>
       <div></div>
     </>
