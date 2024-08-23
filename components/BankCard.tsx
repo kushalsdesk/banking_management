f03f084715b21d79a24f1b097760d7d5f84e71f7
kushalsdesk <kushal99mondal@gmail.com>
2024-08-23 15:43:46 +0530

    add latest bank_card styles

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -19,10 +19,10 @@ const BankCard = ({ account, userName, showBalance }: CreditCardProps) => {
           <article className="flex flex-col gap-2">
             <div className="flex justify-between">
               <h1 className="text-12 font-semibold text-white">{userName}</h1>
-              <h2 className="text-12 font-semibold text-white">** / **</h2>
+              <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
             </div>
-            <p className="text-12 font-semibold tracking-[1.1px] text-white">
-              **** **** **** <span className="text-16">${account.mask}</span>
+            <p className="text-14 font-semibold tracking-[1.1px] text-white">
+              ●●●● ●●●● ●●●● <span className="text-16">1234</span>
             </p>
           </article>
         </div>
@@ -33,6 +33,7 @@ const BankCard = ({ account, userName, showBalance }: CreditCardProps) => {
             width={45}
             height={32}
             alt="mastercard"
+            className="ml-5"
           />
         </div>
         <Image
