9b51d1bcf778b9ceba56449bcd89d04213687412
Kushal Mondal <kushal99mondal@gmail.com>
2024-09-03 23:52:26 +0530

    add customInput to have formSchemas, AuthForm component to have multiple inputs
    both the components are having proper inputs with conditional rendering

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -19,9 +19,10 @@ const AuthForm = ({ type }: AuthFormProps) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  const formSchema = authFormSchema(type);
   // 1. Define your form.
-  const form = useForm<z.infer<typeof authFormSchema>>({
-    resolver: zodResolver(authFormSchema),
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: "",
@@ -29,13 +30,22 @@ const AuthForm = ({ type }: AuthFormProps) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof authFormSchema>) {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     setLoading(true);
-    console.log(values);
-    setLoading(false);
-  }
+    try {
+      //sign up with Appwrite and plaid token
+      if (type === "sign-up") {
+      }
+      if (type === "sign-in") {
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <section className="auth-form">
       <header className="flex flex-col gap-5 md:gap-8">
@@ -67,6 +77,67 @@ const AuthForm = ({ type }: AuthFormProps) => {
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              {type === "sign-up" && (
+                <>
+                  <div className="flex gap-3">
+                    <CustomInput
+                      control={form.control}
+                      name="firstname"
+                      label="First Name"
+                      placeholder="Enter your firstname"
+                    />
+
+                    <CustomInput
+                      control={form.control}
+                      name="lastname"
+                      label="Last Name"
+                      placeholder="Enter your lastname"
+                    />
+                  </div>
+
+                  <CustomInput
+                    control={form.control}
+                    name="address"
+                    label="Address"
+                    placeholder="Enter your specific address"
+                  />
+
+                  <CustomInput
+                    control={form.control}
+                    name="city"
+                    label="City"
+                    placeholder="Enter your city "
+                  />
+                  <div className="flex gap-3">
+                    <CustomInput
+                      control={form.control}
+                      name="state"
+                      label="State"
+                      placeholder="Example: NY"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="postalCode"
+                      label="Postal Code"
+                      placeholder="Example: 11101"
+                    />
+                  </div>
+                  <div className="flex gap-3">
+                    <CustomInput
+                      control={form.control}
+                      name="dateOfBirth"
+                      label="Date of Birth"
+                      placeholder="YYYY-MM-DD"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="ssn"
+                      label="SSN"
+                      placeholder="Example: 1234"
+                    />
+                  </div>
+                </>
+              )}
               <CustomInput
                 control={form.control}
                 name="email"
@@ -81,18 +152,20 @@ const AuthForm = ({ type }: AuthFormProps) => {
                 type="password"
               />
 
-              <Button type="submit" disabled={loading} className="form-btn">
-                {loading ? (
-                  <>
-                    <Loader2 size={20} className="animate-spin" /> &nbsp;
-                    Loading....
-                  </>
-                ) : type === "sign-in" ? (
-                  "Sign In"
-                ) : (
-                  "Sign Up"
-                )}
-              </Button>
+              <div className="flex flex-col gap-4">
+                <Button type="submit" disabled={loading} className="form-btn">
+                  {loading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading....
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Sign In"
+                  ) : (
+                    "Sign Up"
+                  )}
+                </Button>
+              </div>
             </form>
           </Form>
           <footer className="flex justify-center gap-1">
--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -5,9 +5,10 @@ import { z } from "zod";
 import { Control, FieldPath } from "react-hook-form";
 import { authFormSchema } from "@/lib/utils";
 
+const formSchema = authFormSchema("sign-up");
 interface InputProps {
-  control: Control<z.infer<typeof authFormSchema>>;
-  name: FieldPath<z.infer<typeof authFormSchema>>;
+  control: Control<z.infer<typeof formSchema>>;
+  name: FieldPath<z.infer<typeof formSchema>>;
   label: string;
   placeholder: string;
   type?: string;
