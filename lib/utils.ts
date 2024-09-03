944baf4f42fbf36ddda40996a13623aa97476d96
Kushal Mondal <kushal99mondal@gmail.com>
2024-09-03 23:51:20 +0530

    add rest of the form fields to validate with zod formschema
    using conditional to set the fields optional with the formtype

--- lib/utils.ts
+++ lib/utils.ts
@@ -195,7 +195,20 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
-export const authFormSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
-});
+export const authFormSchema = (type: String) =>
+  z.object({
+    //sign-up only
+    firstname: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    lastname: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    address: type === "sign-in" ? z.string().optional() : z.string().max(50),
+    city: type === "sign-in" ? z.string().optional() : z.string().max(50),
+    state:
+      type === "sign-in" ? z.string().optional() : z.string().min(2).max(2),
+    postalCode:
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(6),
+    dateOfBirth: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    ssn: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    //both
+    email: z.string().email(),
+    password: z.string().min(8),
+  });
